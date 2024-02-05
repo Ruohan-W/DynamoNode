4935a5b5e8fd74d8d81a1595e5a9f4cbbc33b09d
Ruohan Wang <Ruohan.Wang@aecom.com>
2024-02-04 17:13:28 -0700

    first commit

---
+++ Grids.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using Autodesk.DesignScript.Runtime;
+using Autodesk.Revit.DB;
+using Revit.Elements;
+using RevitServices.Transactions;
+using RevitServices.Persistence;
+using Autodesk.DesignScript.Geometry;
+using Revit.GeometryConversion;
+
+namespace DynamoNode
+{
+    public class Grids
+    {
+        public static List<Autodesk.DesignScript.Geometry.Rectangle> RectangularGrid(int xCount, int yCount)
+        {
+            double x = 0;
+            double y = 0;
+            var pList = new List<Autodesk.DesignScript.Geometry.Rectangle>();
+
+            for (int i = 0; i < xCount; i++)
+            {
+                y++;
+                x = 0;
+
+                for (int j = 0; j < yCount; j++) 
+                {
+                    x++;
+                    Autodesk.DesignScript.Geometry.Point pt = Autodesk.DesignScript.Geometry.Point.ByCoordinates(x, y);
+                    Autodesk.DesignScript.Geometry.Vector vec = Autodesk.DesignScript.Geometry.Vector.ZAxis();
+                    Autodesk.DesignScript.Geometry.Plane bP = Autodesk.DesignScript.Geometry.Plane.ByOriginNormal(pt, vec);
+                    Autodesk.DesignScript.Geometry.Rectangle rect = Autodesk.DesignScript.Geometry.Rectangle.ByWidthLength(bP, 1, 1);
+                    pList.Add(rect);
+                }
+            }
+
+            return pList;
+        }
+    }
+}
---
+++ HelloWorld.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using Autodesk.DesignScript.Runtime;
+using Autodesk.Revit.DB;
+using Revit.Elements;
+using RevitServices.Transactions;
+using RevitServices.Persistence;
+using Autodesk.DesignScript.Geometry;
+using Revit.GeometryConversion;
+
+namespace DynamoNode
+{
+    public class HelloWorld
+    {
+    }
+}
